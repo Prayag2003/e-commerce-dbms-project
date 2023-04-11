d714514bcc1921aefa83fb8cff07028750d9835c
Prayag <prayagbhatt2003@gmail.com>
2023-04-11 23:01:11 +0530

    Updated App and Database.js

--- database.js
+++ database.js
@@ -1,22 +1,29 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Pool Object --> collection of objects
-const pool = mysql.createPool({
+let pool;
+mysql.createConnection({
     host: process.env.db_host,
     user: process.env.db_user,
     password: process.env.db_password,
     database: process.env.db_database,
-}).promise()
+}).then(p => {
+    pool = p;
+}).catch(err => console.error(err))
 
 export async function getCustomerTable() {
-    const [row] = await pool.query("SELECT * from customer")
-    return row;
+    try {
+        const [row] = await pool.execute("SELECT * from customer")
+        return row;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export async function getCustomer(custID) {
-    const [row] = await pool.query(
+    const [row] = await pool.execute(
         // Passing the Prepared Statement
         `SELECT * from customer 
         where custID = ?` , [custID]
@@ -26,14 +33,14 @@ export async function getCustomer(custID) {
 
 export async function createCustomer(name, email, phoneNo, shippingAddress) {
 
-    const [newCustomer] = await pool.query(`Insert into customer (name , email , phoneNo , shippingAddress ) values ( ? , ? , ? , ? ) 
+    const [newCustomer] = await pool.execute(`Insert into customer (name , email , phoneNo , shippingAddress ) values ( ? , ? , ? , ? ) 
     ` , [name, email, phoneNo, shippingAddress])
     const id = newCustomer.insertId;
     return getCustomer(id);
 }
 
 export async function deleteCustomer(custID) {
-    await pool.query(
+    await pool.execute(
         `Delete from customer 
         where custID = ?`, [custID]
     )
